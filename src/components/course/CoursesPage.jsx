770817cbe02d8e14d5bfe1c2bd228a9f3d4d373e
Mudasar Rauf <mudassar.rauf@gmail.com>
2017-06-29 19:13:31 +0100

    working on courses page

--- src/components/course/CoursesPage.jsx
+++ src/components/course/CoursesPage.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
 class CoursesPage extends Component {
+
+    constructor(props, context){
+        super(props);
+        this.state = { course: {title: null}};
+    }
+
+    onTitleChagne(e) {
+        const course = this.state.course;
+        course.title = e.target.value;
+        this.setState({course: {course}});
+    }
+
+    onClickSave(e){
+        alert(`Saving ${this.state.course.title}`);
+    }
+
     render () {
         return (
             <div>
                 <h1>Courses</h1>
+                <h2>Add Course</h2>
+                <input type="text" onChange={this.onTitleChagne} />
+                <input type="submit" value="Save" onClick={this.onClickSave} />
             </div>
         )
     }

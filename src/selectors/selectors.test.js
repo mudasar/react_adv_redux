c924c77af35ec7ade0b87a1a4762ed05a4f40482
Mudasar Rauf <mudassar.rauf@gmail.com>
2017-07-03 18:45:26 +0100

    last tests files

--- src/selectors/selectors.test.js
+++ src/selectors/selectors.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import expect from 'expect';
 import {mount, shallow} from 'enzyme';
-import { authorFormattedForDropdown } from './selectors';
+import { authorFormattedForDropdown } from './0selectors';
 
 
 describe('Author Selectors', () => {
